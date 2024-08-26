838a363398a0d8b088d0bf652bb625fa6f2821a7
Art3m1sHun <minhhungdenguyn052@gmail.com>
2024-08-26 21:44:29 +0700

    update

--- bai3(set).cpp
+++ bai3(set).cpp
@@ -6,33 +6,38 @@
 using namespace std;
 
 int main() {
+    cout << "nhap so luong test: "<< endl;
     int test; cin >> test;
-    while(test != 0) {
+    while(test--) {
+        int num;
+        cout << "nhap mang so luong 1: " << endl;
         int mang1; cin >> mang1;
+        cout << "nhap mang so luong 2: " << endl;
         int mang2; cin >> mang2;
-        int a1[maxn]; int a2[maxn];
-        set<int> se1;
-        set<int> se2;
-
+        int a1[maxn];
+        set<int> se;
+        cout << "nhap mang 1: "<<endl;
         for (int i = 0; i < mang1; i++){
             cin >> a1[i];
-            se.insert(a1[i]);
         }
+        cout << "nhap mang 2: "<<endl;
         for (int j = 0; j < mang2; j++){
-            cin >> a2[i];
-            se.insert(a2[j]);
+            cin >> num;
+            se.insert(num);
         }
         int dem = 0;
-        for (int i = 0; i < mang1; i++){
-            auto it = se.find(a1[i]);
-            for(int j = 0; j < mang2; j++){
-                if (it != se.end()) {
-                    dem++;
-                }
+        for (auto i = 0; i < mang1; i++) {
+            if (se.find(a1[i]) == se.end()) {
+                dem++;
             }
         }
-        test--;
+
+        if (dem == 0) {
+            cout << "NOT FOUND" << endl;
+        } else {
+            cout << dem << endl;
+        }
     }
+
     return 0;
-    system("pause");
 }
